dc6a55fcc062eca7d486eb3ca197b687ba593e91
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2023-11-24 04:31:28 +0530

    oi

---
+++ RegularEx/Ass.py
@@ -0,0 +1,2 @@
+# Write a program to extract all mobile numbers  and Email ids present in data.txt, where numbers and email ids are mixed with normal text data.
+# Extract all Mobile numbers and Email id and write in new text file, or csv file.
